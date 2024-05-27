31cfded9f7769ab86b96959f85e75176b5089b29
Koye <54333700+kryptcode@users.noreply.github.com>
2024-05-27 22:48:53 +0100

    "auth and chat page"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,39 @@
-import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full py-16 text-white bg-[#163748] ">
+    <main className="min-h-screen w-full py-16 text-white bg-[#163748] ">
       {/* <div className="flex w-[90%] mx-auto lg:w-[70%] mb-10 justify-end">
         <button className="p-1.5 px-5 rounded-md font-medium bg-[#69f2fa] text-[#163748] hover:opacity-80 transition-opacity duration-300 ease-linear ">Login</button>
       </div> */}
       <div className="flex flex-col-reverse lg:flex-row w-[90%] mx-auto lg:w-[70%] h-[80%]">
         <div className="flex-1">
-          <h3 className="text-4xl lg:text-5xl font-semibold lg:leading-normal mb-5">Your Personal AI<br /> Mental Health <br /> <span>Chatbot!</span>  </h3>
-          <button className="p-1.5 px-5 rounded-md font-medium bg-[#69f2fa] text-[#163748] text-lg hover:opacity-80 transition-opacity duration-300 ease-linear">Get Started</button>
+          <h3 className="text-4xl lg:text-5xl font-semibold lg:leading-normal mb-5">
+            Your Personal AI
+            <br /> Mental Health <br />{" "}
+            <span className="text-[#69f2fa]">Chatbot!</span>{" "}
+          </h3>
+          {/* <button className="p-1.5 px-5 rounded-md font-medium bg-[#69f2fa] text-[#163748] text-lg hover:opacity-80 transition-opacity duration-300 ease-linear">
+            Get Started
+          </button> */}
+
+          <div>
+            <SignedOut>
+              <button className="p-1.5 px-5 rounded-md font-medium bg-[#69f2fa] text-[#163748] text-lg hover:opacity-80 transition-opacity duration-300 ease-linear">
+
+              <SignInButton  />
+              </button>
+            </SignedOut>
+            <SignedIn>
+              <button className="p-1.5 px-5 rounded-md font-medium bg-[#69f2fa] text-[#163748] text-lg hover:opacity-80 transition-opacity duration-300 ease-linear">
+                Continue chatting
+              </button>
+            </SignedIn>
+          </div>
         </div>
         <div className="flex-1 h-full p-10">
           <img
-            src={'/Group 22.png'}
+            src={"/Group 22.png"}
             alt="Bot saying Can I help you "
             className="w-full object-contain"
           />
