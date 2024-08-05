4d676d528860a1245006dacffb5d0777198d5cad
Koye <54333700+kryptcode@users.noreply.github.com>
2024-08-05 13:53:42 +0100

    "original-chat"

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -33,49 +33,49 @@ const ChatPage = () => {
   //   setInput("");
   // };
 
-  return (
-    <div>
-      <ChatBot />
-    </div>
-  )
-
   // return (
-  //   <main className="h-screen flex flex-col bg-[#ff4f5a] w-full">
-  //     <div className=" py-5 h-[15vh] ">
-  //       <h5 className=" text-center text-lg font-medium">Ọ̀rẹ́ Ọkàn</h5>
-  //       <div className="flex space-x-1.5 text-sm mt-2 items-center justify-center">
-  //         <div className="h-2 w-2 rounded-full bg-green-600" />
-  //         <span>Online</span>
-  //       </div>
-  //     </div>
+  //   <div>
+  //     <ChatBot />
+  //   </div>
+  // )
 
-  //     <div className="flex-grow w-[90%] md:w-[70%] mx-auto flex flex-col h-[85vh]">
+  return (
+    <main className="h-screen flex flex-col bg-[#ff4f5a] w-full">
+      <div className=" py-5 h-[15vh] ">
+        <h5 className=" text-center text-lg font-medium">Ọ̀rẹ́ Ọkàn</h5>
+        <div className="flex space-x-1.5 text-sm mt-2 items-center justify-center">
+          <div className="h-2 w-2 rounded-full bg-green-600" />
+          <span>Online</span>
+        </div>
+      </div>
 
-  //     <div className="flex-grow bg-[#fff] rounded-lg overflow-y-scroll">
-  //       {messages.map((msg, index) => (
-  //         <div className="space-y-4" key={index}>
-  //           <p>
-  //             <strong>You:</strong> {msg.user}
-  //           </p>
-  //           <p>
-  //             <strong>Bot:</strong> {msg.bot}
-  //           </p>
-  //         </div>
-  //       ))}
-  //     </div>
-  //     <div className="flex justify-between py-3 lg:w-[70%] space-x-8 mx-auto ">
-  //     <input
-  //       type="text"
-  //       value={input}
-  //       className="bg-neutral-200 w-full px-5 py-1 rounded-md"
-  //       placeholder="Ask me anything..."
-  //       onChange={(e) => setInput(e.target.value)}
-  //     />
-  //     <button onClick={sendMessage} className="bg-[#163748] text-white px-5 py-1 rounded-md ">Send</button>
-  //     </div>
-  //     </div>
-  //   </main>
-  // );
+      <div className="flex-grow w-[90%] md:w-[70%] mx-auto flex flex-col h-[85vh]">
+
+      <div className="flex-grow bg-[#fff] rounded-lg overflow-y-scroll">
+        {messages.map((msg, index) => (
+          <div className="space-y-4" key={index}>
+            <p>
+              <strong>You:</strong> {msg.user}
+            </p>
+            <p>
+              <strong>Bot:</strong> {msg.bot}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-between py-3 lg:w-[70%] space-x-8 mx-auto ">
+      <input
+        type="text"
+        value={input}
+        className="bg-neutral-200 w-full px-5 py-1 rounded-md"
+        placeholder="Ask me anything..."
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={sendMessage} className="bg-[#163748] text-white px-5 py-1 rounded-md ">Send</button>
+      </div>
+      </div>
+    </main>
+  );
 };
 
 export default ChatPage;
